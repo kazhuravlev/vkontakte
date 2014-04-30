044e09662c068438efb2b06e0bfb4fbab2aa415c
Chewey <prosto-chewey@users.noreply.github.com>
2014-04-30 13:12:09 +0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     long_description = open('README.rst').read() + open('CHANGES.rst').read(),
 
     classifiers=[
-        'Development Status :: 4',
+        'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
         'Framework :: Django',
         'Intended Audience :: Developers',
